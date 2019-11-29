49bd22c999a4872f7a06d46933fcc2a191537ef2
LuisLSousa <luis.srl.sousa@gmail.com>
2019-11-29 20:06:44 +0000

    reproduce_Moran() not working properly, not used yet.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class evolutionIndirectReciprocitySimulation:
 
     nodes = []
     validNodeIds = []
-    
+
     def __init__(self, numNodes, numInteractions, numGenerations, initialScore=0,
                  benefit=1, cost=0.1, strategyLimits=[-5,6], scoreLimits=[-5,5], mutation=False, logFreq=3):
 
@@ -36,87 +36,137 @@ class evolutionIndirectReciprocitySimulation:
         self.initiateNodes()
 
     def runSimulation(self):
-        print('=====    Initiating simulation   ======')
+        # print('=====    Initiating simulation   ======')
         perGenLogs = []
         for i in range(self.numGenerations):
-            print('-- Generation {} --'.format(i))
+            # print('-- Generation {} --'.format(i))
             self.runGeneration()
 
             # self.printPayoffs()
-            self.reproduce()
             if i%self.logFreq==0:
-                print('== Logging {} =='.format(i))
+                # print('== Logging {} =='.format(i))
                 l = self.perGenLogs(i)
                 perGenLogs.append(l)
 
+            self.reproduce()
+            #self.reproduce_Moran()
+
         self.finalLogs(perGenLogs)
-        breakpoint()
 
     def runGeneration(self):
         interactionPairs = self.pickInteractionPairs(self.nodes, self.numInteractions)
         for pair in interactionPairs:
             self.runInteraction(pair)
 
-        # print(self.nodes)
+        # # print(self.nodes)
         return {}
 
     def runInteraction(self, pair):
-        donner = pair[0]
+        donor = pair[0]
         recipient = pair[1]
         score = self.checkRecipientScore(recipient)
 
-        if score >= donner['strategy']:      # Each node has its own strategy
+        if score >= donor['strategy']:      # Each node has its own strategy
             # Cooperate
-            if[donner['score'] < self.scoreLimits[1]]:
-                donner['score'] += 1
-            donner['payoff'] -= self.payoffCost
-            donner['payoff'] += self.payoffBenefit
-
-            # todo - talk with the teacher to make sure this is right
-            donner['score'] += 0.1
-            recipient['score'] += 0.1
+            if[donor['score'] < self.scoreLimits[1]]:
+                donor['score'] += 1
+            donor['payoff'] -= self.payoffCost
+            recipient['payoff'] += self.payoffBenefit
 
         else:
             # Deflect
-            if[donner['score'] > self.scoreLimits[0]]:
-                donner['score'] -= 1
+            if[donor['score'] > self.scoreLimits[0]]:
+                donor['score'] -= 1
+
+        # todo - talk with the teacher to make sure this is right
+        donor['payoff'] += 0.1
+        #recipient['score'] += 0.1
+
         return
 
     def reproduce(self):
-        print('== Raising new generation ==')
+        # print('== Raising new generation ==')
         newNodes = []
 
         payoffs = [node['payoff'] for node in self.nodes]
         totalPayoff = sum(payoffs)
 
         numChilds = [p*self.numNodes/totalPayoff for p in payoffs]
-        # print(payoffs)
+        # # print(payoffs)
         numChilds = self.round_series_retain_integer_sum(numChilds)
 
         for i, node in enumerate(self.nodes):
             offspring = numChilds[i]
-            # print('{} - {}'.format(numChilds[i], offspring))
-            # print('Reproducing {}'.format(offspring))
+            # # print('{} - {}'.format(numChilds[i], offspring))
+            # # print('Reproducing {}'.format(offspring))
             for c in range(offspring):
                 newNode = node.copy()
                 newNode['score'] = 0
+                newNode['payoff'] = 0
                 newNode['id'] = self.idIterator
                 self.idIterator += 1
 
                 if self.mutation:
                     if self.casino(0.001):
                     # if self.casino(0.2):
-                        print('JACKPOT')
+                        # print('JACKPOT')
                         newNode['strategy'] = random.randrange(self.strategyLimits[0], self.strategyLimits[1]+1)
 
                 newNodes.append(newNode)
             else:
-                # print('Not reproducing :( ')
+                # # print('Not reproducing :( ')
                 pass
         self.nodes = newNodes
 
-        print('Size of new generation is {}'.format(len(self.nodes)))
-        # print(self.nodes)
+        # print('Size of new generation is {}'.format(len(self.nodes)))
+        # # print(self.nodes)
+
+    def reproduce_Moran(self):
+        # print('== Moran in the House ==')
+        newNodes = []
+        threshold = []
+        strat = []
+
+        payoffs = [node['payoff'] for node in self.nodes]
+        #totalPayoff = sum(payoffs)
+
+        #numChilds = [p*self.numNodes/totalPayoff for p in payoffs]
+        # # print(payoffs)
+
+        totalPayoff = 0
+        for p in payoffs:
+            totalPayoff += p
+            if p > 0:
+                threshold.append(totalPayoff)
+
+        breakpoint()
+
+        for i, node in enumerate(self.nodes):
+            offspring = numChilds[i]
+            # # print('{} - {}'.format(numChilds[i], offspring))
+            # # print('Reproducing {}'.format(offspring))
+            for c in range(offspring):
+                newNode = node.copy()
+                newNode['score'] = 0
+                newNode['payoff'] = 0
+                newNode['id'] = self.idIterator
+                self.idIterator += 1
+
+                if self.mutation:
+                    if self.casino(0.001):
+                    # if self.casino(0.2):
+                        # print('JACKPOT')
+                        newNode['strategy'] = random.randrange(self.strategyLimits[0], self.strategyLimits[1]+1)
+
+                newNodes.append(newNode)
+            else:
+                # # print('Not reproducing :( ')
+                pass
+        self.nodes = newNodes
+
+        # print('Size of new generation is {}'.format(len(self.nodes)))
+        # # print(self.nodes)
+
 
     def round_series_retain_integer_sum(self, xs):
         N = sum(xs)
@@ -133,7 +183,7 @@ class evolutionIndirectReciprocitySimulation:
         # todo - We should guarantee that each pair interacts once at most.
         # todo - never with the same partner twice (doesnt matter if in different roles)
 
-        print("> Calculating interaction pairs ")
+        # print("> Calculating interaction pairs ")
 
         # Generate all possible non-repeating pairs
         '''pairs = list(itertools.combinations(ids, 2))
@@ -149,8 +199,8 @@ class evolutionIndirectReciprocitySimulation:
                 rand2 = random.choice(ids)
             pair = [rand1, rand2]
             pairs.append(pair)
-        # print(pairs)
-        print("- {} pairs calculated".format(len(pairs)))
+        # # print(pairs)
+        # print("- {} pairs calculated".format(len(pairs)))
         return pairs
 
     def checkRecipientScore(self, node):
@@ -158,9 +208,9 @@ class evolutionIndirectReciprocitySimulation:
         return node['score']
 
     def perGenLogs(self, it):
-        print('== Logging Results ==')
+        # print('== Logging Results ==')
 
-        # print(self.nodes)
+        # # print(self.nodes)
 
         # Strategy Distribution
         strategies = [n['strategy'] for n in self.nodes]
@@ -216,12 +266,13 @@ class evolutionIndirectReciprocitySimulation:
             print(node['payoff'])
 
     def casino(self, percentage):
-        return (random.randrange(100) < percentage);
+        return (random.random() < percentage);
 
 
 if __name__ == "__main__":
     # Original paper values:
     originalPaperValues = {
+        'logFreq': 1,
         'numNodes': 100,
         'numInteractions':  125,
         'numGenerations': 200,
